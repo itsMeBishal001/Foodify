e1b2f0a69256ebd5c805175e75843a09b3c16be5
BISHAL DATTA <dattabishal001@gmail.com>
2023-02-12 09:06:10 +0530

    igniting our app using npm,parcel,react,react-dom

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
-// <!-- using js -->
+import React from "react";
+import ReactDOM from "react-dom";
 
-//       //   const heading = document.getElementById("root");
-
-//       const h1 = document.createElement("h1");
-//       const node = document.createTextNode("hello with js");
-
-//       h1.appendChild(node);
-//       document.getElementById("root").appendChild(h1);
-//       //   const h = document.createElement("h1");
-//       //   heading.appendChild=(h);
-//       //   heading.innerHTML = "<h1>hello with js</h1>";
-
-//     <!-- using react -->
 const heading = React.createElement("h1", {}, "hello with react");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);

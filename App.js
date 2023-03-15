37ac2fc926d5c646deb209ccc3cdce863afe2c93
BISHAL DATTA <dattabishal001@gmail.com>
2023-03-15 11:07:04 +0530

    using jsx
    did the same using jsx

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const heading1 = <h1 className="title"> hello from h1</h1>;
+const heading2 = <h2 className="title"> hello from h2</h2>;
+const heading3 = <h3 className="title"> hello from h3</h3>;
 
-const heading1 = React.createElement("h1", {className:"title"}, "hello from h1");
-const heading2 = React.createElement("h2", {className:"title"}, "hello from h2");
-const heading3 = React.createElement("h3", {className:"title"}, "hello from h3");
-const heading = React.createElement("div", {}, [heading1,heading2,heading3,"hello from parent folder"]);
+const heading = (
+  <div>
+    {heading1}
+    {heading2}
+    {heading3}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);

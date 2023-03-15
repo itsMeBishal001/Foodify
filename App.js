4d9146f77fd21b7d9239f6e20340c72c59f74aea
BISHAL DATTA <dattabishal001@gmail.com>
2023-03-15 10:44:06 +0530

    same

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 const heading1 = React.createElement("h1", {className:"title"}, "hello from h1");
 const heading2 = React.createElement("h2", {className:"title"}, "hello from h2");
 const heading3 = React.createElement("h3", {className:"title"}, "hello from h3");
-const heading = React.createElement("div", {}, [heading1,heading2,heading3]);
+const heading = React.createElement("div", {}, [heading1,heading2,heading3,"hello from parent folder"]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);

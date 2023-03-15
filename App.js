b4f1797a4daa97fed2a72a113853b2833f862435
BISHAL DATTA <dattabishal001@gmail.com>
2023-03-15 10:41:56 +0530

    React.createElement
    created a nested header element using React.createElement h1,h2,h3 inside din as a class name title

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1", {}, "hello with react");
+
+const heading1 = React.createElement("h1", {className:"title"}, "hello from h1");
+const heading2 = React.createElement("h2", {className:"title"}, "hello from h2");
+const heading3 = React.createElement("h3", {className:"title"}, "hello from h3");
+const heading = React.createElement("div", {}, [heading1,heading2,heading3]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);

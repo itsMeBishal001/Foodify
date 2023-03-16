f55ae6267b3822386e167a546831426acf4871e6
BISHAL DATTA <dattabishal001@gmail.com>
2023-03-16 10:26:16 +0530

    composition of comonent
    component inside another component

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-//jsx expression
-//internally jsx doing the React.createElement and returning an object
-const heading1 = <h1 className="title"> hello from h1</h1>;
-const heading2 = <h2 className="title"> hello from h2</h2>;
-const heading3 = <h3 className="title"> hello from h3</h3>;
+//jsx expression to functional component
+
+const Heading1 = () => {
+  return (<h1 className="title">hello from h1</h1>);
+};
+const Heading2 = () => {
+  return (<h2 className="title">hello from h2</h2>);
+};
+const Heading3 = () => {
+  return( <h3 className="title">hello from h3</h3>);
+};
+
 
 // component -> functional component && classbased compontnt
 //functional component
 const Heading = () => {
   return (
-    /* in this return i have to return only one tag --if we don't want to show 
-     any extra tag on the inspect page then use React.Fragment 
-     */
     <>
-      {heading1}
-      {heading2}
-      {heading3}
+      <Heading1 />
+      <Heading2 />
+      <Heading3 />
+      
     </>
   );
 };

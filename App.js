b6e5b26d9699b527824241006b84a4dc6c5a0f31
BISHAL DATTA <dattabishal001@gmail.com>
2023-03-16 10:11:53 +0530

    introduced with functional componrnt
    created the same using functional component with JSX

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//jsx expression
+//internally jsx doing the React.createElement and returning an object
 const heading1 = <h1 className="title"> hello from h1</h1>;
 const heading2 = <h2 className="title"> hello from h2</h2>;
 const heading3 = <h3 className="title"> hello from h3</h3>;
 
-const heading = (
-  <div>
-    {heading1}
-    {heading2}
-    {heading3}
-  </div>
-);
+// component -> functional component && classbased compontnt
+//functional component
+const Heading = () => {
+  return (
+    /* in this return i have to return only one tag --if we don't want to show 
+     any extra tag on the inspect page then use React.Fragment 
+     */
+    <>
+      {heading1}
+      {heading2}
+      {heading3}
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+// can call  a functional component ==> <component/> && component()-- because it is a function
+root.render(<Heading />);

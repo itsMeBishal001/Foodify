08eb82f85b8f566a6dae9221116aa13c3e52a128
BISHAL DATTA <dattabishal001@gmail.com>
2023-03-21 13:16:09 +0530

    Is JSX mandatory for React? && How can I write comments in JSX?

--- App.js
+++ App.js
@@ -4,24 +4,29 @@ import ReactDOM from "react-dom/client";
 //jsx expression to functional component
 
 const Logo = () => {
-  return (<h1 className="logo">A user icon</h1>);
+  return <h1 className="logo">A user icon</h1>;
 };
 
-const search=<input className="search" type="text" />;
- 
-const image=<img className="image" src="https://penji.co/wp-content/uploads/2022/08/5.-food-panda-logo.jpg"/>
+const search = <input className="search" type="text" />;
 
-
+const image = (
+  <img
+    className="image"
+    src="https://penji.co/wp-content/uploads/2022/08/5.-food-panda-logo.jpg"
+  />
+);
 
 // component -> functional component && classbased compontnt
 //functional component
 const Heading = () => {
   return (
     <div className="heading">
-     {image}
-     {search}
+      {
+        // writing comments on jsx =>i can make comments within {} .. as here is js section
+      }
+      {image}
+      {search}
       <Logo />
-      
     </div>
   );
 };
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lets Start with React
  updating all the staf related my react learning journy
+## Is JSX mandatory for React?
+jsx is not mandatory for easy to write our code jsx is used.
+=>JSX by using React.createElement() and make it easy and less code and maintainable

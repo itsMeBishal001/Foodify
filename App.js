c0e41fe62625b40b7614881ebcb3e6561a67aa55
BISHAL DATTA <dattabishal001@gmail.com>
2023-03-22 12:19:37 +0530

    build

--- App.js
+++ App.js
@@ -1,32 +1,84 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//jsx expression to functional component
+/*
+ * App layout --
+ *  header -
+ *    logo
+ *    home about cart
+ *  body -
+ *     search
+ *     restrudnt list-
+ *         restrudent card 
+ *            image rating name cuisins
+ *  footer  
+ */
 
-const Logo = () => {
-  return <h1 className="logo">A user icon</h1>;
-};
-
-const search = <input className="search" type="text" />;
-
-const image = (
+const logo = (
   <img
     className="image"
-    src="https://penji.co/wp-content/uploads/2022/08/5.-food-panda-logo.jpg"
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjLki7JKI12Q_QEUwR0qtnPoi3oboOMI4xCA&usqp=CAU"
   />
 );
 
 // component -> functional component && classbased compontnt
 //functional component
+
 const Heading = () => {
   return (
     <div className="heading">
       {
         // writing comments on jsx =>i can make comments within {} .. as here is js section
       }
-      {image}
-      {search}
-      <Logo />
+      {logo}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+      
+    </div>
+  );
+};
+const RestrudentCard=()=>{
+  return(
+    <div className="restrurdentCard">
+    <img className ="cardImg"src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/iflof6e8fmd9nwmwg0nf"
+    />
+    <h3>Birini By Kilo</h3>
+    <h4>Biryani, Hyderabadi</h4>
+    <h4>Durgapur Locality 6.0 km</h4>
+
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      
+      {<RestrudentCard/>}
+      
+    </div>
+  );
+};
+const Footer = () => {
+  return (
+    <div className="footer">
+      footer
+      
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="applayout">
+       {<Heading/>}
+       {<Body/>}
+       {<Footer/>}
+      
     </div>
   );
 };
@@ -34,4 +86,4 @@ const Heading = () => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // can call  a functional component ==> <component/> && component()-- because it is a function
-root.render(<Heading />);
+root.render(<AppLayout />);

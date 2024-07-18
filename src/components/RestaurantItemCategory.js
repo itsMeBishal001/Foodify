27473861c88eeb3068864793e3f5ff6737f93ce9
BISHAL DATTA <dattabishal001@gmail.com>
2024-07-18 18:34:05 +0530

    refactor: Moved restaurant menu data fetching to a custom hook; improved UI for dishes display.

--- src/components/RestaurantItemCategory.js
+++ src/components/RestaurantItemCategory.js
@@ -1,20 +1,33 @@
-import RestaurantDish from "./RestaurantDish";
+import { useState } from 'react';
+import RestaurantDish from './RestaurantDish';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 const RestaurantItemCategory = (itemCategory) => {
+  const [showDishes, setShowDishes] = useState(false);
+
+  const toggleDishes = () => {
+    setShowDishes(!showDishes);
+  };
+
   return (
     <div>
-      <h1 className="dishHeader text-2xl font-bold mb-4">
-        {itemCategory?.title} ({itemCategory?.itemCards.length})
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {itemCategory?.itemCards?.map((dish, index) => {
-          return (
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="dishHeader text-2xl font-bold">
+          {itemCategory?.title} ({itemCategory?.itemCards.length})
+        </h1>
+        <button onClick={toggleDishes} className="text-blue-600 w-6">
+          {showDishes ? <FaChevronUp /> : <FaChevronDown />}
+        </button>
+      </div>
+      {showDishes && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {itemCategory?.itemCards?.map((dish, index) => (
             <div key={index}>
               <RestaurantDish {...dish?.card?.info} />
             </div>
-          );
-        })}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/RestaurantNestedItemCategory.js
+++ src/components/RestaurantNestedItemCategory.js
@@ -1,22 +1,36 @@
+import { useState } from 'react';
 import RestaurantDish from "./RestaurantDish";
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 const RestaurantCategory = (category) => {
+  const [showDishes, setShowDishes] = useState(false);
+
+  const toggleDishes = () => {
+    setShowDishes(!showDishes);
+  };
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">{category?.title}</h1>
       {category?.categories?.map((groupedSubCategory, index) => (
         <div key={index} className="mb-8">
-          <h2 className="text-xl font-bold mb-2">
-            {groupedSubCategory?.title} ({groupedSubCategory?.itemCards?.length})
-          </h2>
+          <div className="flex items-center justify-between mb-4">
+            <h2 className="text-xl font-bold mb-2">
+              {groupedSubCategory?.title} ({groupedSubCategory?.itemCards?.length})
+            </h2>
+            <button onClick={toggleDishes} className="text-blue-600 w-6">
+              {showDishes ? <FaChevronUp /> : <FaChevronDown />}
+            </button>
+          </div>
           <div className="border-b-2 border-gray-300 mb-4"></div>
-          <ul>
-            {groupedSubCategory?.itemCards?.map((dish, index) => (
-              <li key={index} className="mb-4">
-                <RestaurantDish {...dish?.card?.info} />
-              </li>
-            ))}
-          </ul>
+          {showDishes && (
+            <ul>
+              {groupedSubCategory?.itemCards?.map((dish, index) => (
+                <li key={index} className="mb-4">
+                  <RestaurantDish {...dish?.card?.info} />
+                </li>
+              ))}
+            </ul>)}
         </div>
       ))}
     </div>
